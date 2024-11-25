ebc6c88d794df1414efe32cbd3970243161abbd9
Shrimp_RedCake <nicolebatistabonbon@gmail.com>
2024-11-25 11:39:50 -0300

    Add files via upload

---
+++ trabalhinho/exemplo/script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('mybutton');
+const text = document.getElementById('mytext')
+
+button.addEventListener('click', function(){
+    text.textContent ='botaaaaaaaaao';
+    text.style.color = 'blue'
+})
