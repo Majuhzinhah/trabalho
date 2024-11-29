52d4caa7dd7547a0d9087ca3a6307a62d68615c1
Shrimp_RedCake <nicolebatistabonbon@gmail.com>
2024-11-29 08:52:52 -0300

    DELETED

--- trabalhinho/exemplo/script.js
+++
@@ -1,7 +0,0 @@
-const button = document.getElementById('mybutton');
-const text = document.getElementById('mytext')
-
-button.addEventListener('click', function(){
-    text.textContent ='botaaaaaaaaao';
-    text.style.color = 'blue'
-})
